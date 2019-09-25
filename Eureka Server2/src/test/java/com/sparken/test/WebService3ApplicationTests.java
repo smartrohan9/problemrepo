d139701d791b325e7b112816aaac4e75eb4229f7
root <root@appa>
2019-09-25 15:31:39 +0530

    Eureka project

---
+++ Eureka Server2/src/test/java/com/sparken/test/WebService3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sparken.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebService3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
